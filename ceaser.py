a67ba77a9049c2700aebc18eb3941bc3a47fe4b5
Bora Turan <boraturan1@gmail.com>
2016-12-12 15:52:36 +0300

    Comitted for dev purposes bo achievements yet

--- ceaser.py
+++ ceaser.py
@@ -1,7 +1,16 @@
-def shift(word, num):
-    alph = "abcdefghijklmnopqrstuvwxyz"
+lang = input("""Ingilicce: EN
+             Turkce: TR\n"""
+def shift(word, num, way):
+    if lang == "EN":
+        alph = "abcdefghijklmnopqrstuvwxyz"
+    elif lang == "TR":
+        alph = "abc1defg2h3ijklmno4prs5tyvu6vyz"
     indices = [alph.index(char) + num for char in word]
-    indices = [index - (index // 26) * 26 if index >= 26 else index for index in indices]
+    if way is True:
+        indices = [index - (index // len(alph)) * len(alph) if index >= len(alph) else index for index in indices]
+    elif way is False:
+        indices = [index + (index // len(alph)) * len(alph) if index >= len(alph) else index for index in indices]
+
     crypto = "".join([alph[index] for index in indices])
     return crypto
 
@@ -16,31 +25,64 @@ def isalpha(word):
             pass
     return not has_num
 
-print("""Sezar sifrelmesi:
-Lutfen bir kelime ve bir kaydirma sayisi saglayiniz.\n""")
+way = bool(int(input("Sifrelemek icin 1 e,\nSifre donusturmek icin 0 a basiniz")))
 
-while True:
-    num = input("Sayi: ")
-    try:
-        num = int(num)
-        break
-    except ValueError:
-        print("Lutfen bir sayi giriniz")
 
-result = []
-go_on = True
-while go_on is True:
-    word = "123"
-    while not isalpha(word):
-        word = input("Kelime: ")
-        if isalpha(word) == False:
-            print("Lutfen kelimenizde sayi ya da ozel karakter kullanmayiniz!")
-    word = word.lower()
-    result.append(shift(word, num))
+if way is True:
+    print("""Sezar sifrelmesi:
+    Lutfen bir kelime ve bir kaydirma sayisi saglayiniz.\n""")
+
     while True:
+        num = input("Sayi: ")
         try:
-            go_on = bool(int(input("Devam etmek icin herhangi bir sayiya,\nDurmak icin 0a basiniz: ")))
+            num = int(num)
             break
         except ValueError:
-            print("Lutfen sadece sayi giriniz!")
+            print("Lutfen bir sayi giriniz\n")
+
+    result = []
+    go_on = True
+    while go_on is True:
+        word = "123"
+        while not isalpha(word):
+            word = input("Kelime: ")
+            if isalpha(word) == False:
+                print("Lutfen kelimenizde sayi ya da ozel karakter kullanmayiniz!")
+        word = word.lower()
+        result.append(shift(word, num, way))
+        while True:
+            try:
+                go_on = bool(int(input("Devam etmek icin herhangi bir sayiya,\nDurmak icin 0a basiniz: ")))
+                break
+            except ValueError:
+                print("Lutfen sadece sayi giriniz!")
+
+if way is False:
+    print("""Seazar Sifresi Cozme:
+          Sifreli Kelimeyi giriniz
+          Bilgisayar muhtemel sonuclari gosterecektir""")
+
+    while go_on is True:
+     while True:
+        num = input("Sayi: ")
+        try:
+            num = int(num)
+            break
+        except ValueError:
+            print("Lutfen bir sayi giriniz\n")
+
+       word = "123"
+        while not isalpha(word):
+            word = input("Kelime: ")
+            if isalpha(word) == False:
+                print("Lutfen kelimenizde sayi ya da ozel karakter kullanmayiniz!")
+        word = word.lower()
+        result.append(shift(word, num, way))
+        while True:
+            try:
+                go_on = bool(int(input("Devam etmek icin herhangi bir sayiya,\nDurmak icin 0a basiniz: ")))
+                break
+            except ValueError:
+                print("Lutfen sadece sayi giriniz!")
+
 print(" ".join(result))
